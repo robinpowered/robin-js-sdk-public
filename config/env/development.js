47e328b3bc32961ebbcf558e1e4531ff2a4d428d
Mark Costello <mark@markcostello.net>
2014-04-22 18:24:12 -0400

    Add changes for grid

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ var config;
 
 config = {
   apiUrl: 'http://api.test.robinpowered.com/v1.0',
-  gridUrl: 'http://grid.test.robinpowered.com/'
+  gridUrl: 'http://grid.test.robinpowered.com'
 };
 
 module.exports = config;
