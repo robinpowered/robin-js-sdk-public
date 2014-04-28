d173642a28f0a71de9066b51252ea606a9513cc4
Mark Costello <mark@onemightyroar.com>
2014-04-28 15:23:28 -0400

    Updated docs

--- doc/assets/behavior.js
+++ doc/assets/behavior.js
@@ -43,6 +43,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
                 "slug": "robin-grid"
               },
               "depth": 3
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "Development Roadmap",
+                "slug": "development-roadmap"
+              },
+              "depth": 2
             }
           ]
         },
@@ -66,6 +74,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
             "slug": "robin-grid"
           },
           "depth": 3
+        }, {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Development Roadmap",
+            "slug": "development-roadmap"
+          },
+          "depth": 2
         }
       ]
     }, {
